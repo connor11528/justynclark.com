2aaa175a6bcdc316d70911e4d1303c556649a6b7
ijustynclark@gmail.com <jjohnson@historynet.com>
2016-09-19 06:10:10 -0700

    Move byline below blog text and add margin below feature image

--- parts/loop-archive.php
+++ parts/loop-archive.php
@@ -1,12 +1,13 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">					
 	<header class="article-header">
 		<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-		<?php get_template_part( 'parts/content', 'byline' ); ?>
+
 	</header> <!-- end article header -->
 					
 	<section class="entry-content" itemprop="articleBody">
 		<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('full'); ?></a>
 		<?php the_content('<button class="tiny">' . __( 'Read more...', 'jointswp' ) . '</button>'); ?>
+		<?php get_template_part( 'parts/content', 'byline' ); ?>
 	</section> <!-- end article section -->
 						
 	<footer class="article-footer">
