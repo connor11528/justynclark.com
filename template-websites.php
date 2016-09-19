6b36075813266fcbe95964791ec2add101c6c8e7
ijustynclark@gmail.com <jjohnson@historynet.com>
2016-09-19 15:53:22 -0700

    Create websites page template
     - Add new website portfolio item
     - Add websites image thumbnail css hover animation

---
+++ template-websites.php
@@ -0,0 +1,192 @@
+<?php
+/*
+Template Name: WEBSITES
+*/
+?>
+
+<?php get_header(); ?>
+			
+	<div id="content">
+	
+		<div id="inner-content" class="row">
+	
+		    <main id="main" class="large-12 medium-12 columns" role="main">
+
+				<section class="light-bg"><article class="website-example row wide">
+						<div class="large-6 columns">
+							<h3>LenmarLED Enterprises</h3>
+<p>A 50 year old family owned company delivering great value and quality products as a top choice for replacement batteries, power solutions, and innovative technologies. Provided with mockup designs and wireframes I cut graphics elements and applied them to the selected Bootstrap framework and coded responsive, cross browser supported HTML5, CSS3 with various jQuery/JavaScripts seated on ASP.NET platform.</p>
+<p><a id="sldbt-blk-2" class="secondary small button" href="http://www.lenmarled.com/default" target="_blank">Take a Closer Look »</a></p>
+						</div>
+						<div class="large-6 columns">
+							<div class="row">
+								<div class="small-4 columns"><img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/Lenmar-Mobile.jpg" alt="Lenmar-Mobile" /></div>
+								<div class="small-8 columns websites">
+									<div class="websites-thmb-tri"><img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/Lenmar-Desktop.jpg" alt="Lenmar-Desktop" /></div>
+								</div>
+							</div>
+						</div>
+					</article>
+
+					<hr />
+
+					<article class="website-example row wide">
+						<div class="large-6 columns">
+							<h3>In His Presence Church</h3>
+<p>A popular highly acclaimed non-denominational church organization offering powerful praise and worship services and resources.This newly redesigned website brought their internet web presentation into the 21st century. Built with speed and responsiveness it is mobilized to perfection to make user engagement pleasant across multiple desktop and mobile devices on all modern browsers. Use of high quality images allow visitors to truly SEE what this church is about. A solid functioning and user friendly donation solution is implemented allowing visitors give Tithes and offering anytime of day and on service days where visitors can watch via live stream.</p>
+<p><a id="sldbt-blk-2" class="secondary small button" href="http://www.ihpchurch.org/" target="_blank">Take a Closer Look »</a></p>
+						</div>
+						<div class="large-6 columns">
+							<div class="row">
+								<div class="small-4 columns"><img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/IHPC-Mobile-Thumb2.jpg" alt="IHPC Mobile" /></div>
+								<div class="small-8 columns websites">
+									<div class="websites-thmb-tri"><img src="http://www.justynclark.com/wp-content/uploads/2015/01/IHPC-Desktop-Thumb.jpg" alt="IHPC Desktop" /></div>
+								</div>
+							</div>
+						</div>
+					</article>
+
+					<hr />
+
+					<article class="website-example row wide">
+						<div class="large-6 columns">
+							<h3>Fitness Kingdom</h3>
+<p>There’s nothing small about Fitness Kingdom and either is their online eCommerce website. Inspired by the belief that fitness is a way of life and bigger than just one individual, Fitness Kingdom has created an American made line with true quality and a motivational spirit. Built with speed and responsiveness, this websites allows customers to purchase products on mobile with ease and simplicity.</p>
+<p><a id="sldbt-blk-2" class="secondary small button" href="http://fitness-kingdom.com/" target="_blank">Take a Closer Look »</a></p>
+						</div>
+						<div class="large-6 columns">
+							<div class="row">
+								<div class="small-4 columns"><img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/FK-mobile.jpg" alt="FK-mobile" /></div>
+								<div class="small-8 columns websites">
+
+									<img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/Fitness-Kingdom-thmb.jpg" alt="Fitness-Kingdom-thmb" />
+									<div class="websites-thmb-tri"></div>
+								</div>
+							</div>
+						</div>
+					</article>
+
+					<hr />
+
+					<article class="website-example row wide">
+						<div class="large-6 columns">
+							<h3>Glitter&amp;Mud</h3>
+<p>Glitter&amp;Mud Travel, an adventure website blog by Vanessa Rivers created to inspire you to live life to the fullest and make the most of all your travels. It’s hard to fault Glitter&amp;Mud for her desire to make the site friendly towards mobile devices. But it’s the responsiveness in the layout and the crisp photography that makes us want to go out and trek the world. Watch the homepage slider animate or scale the browser down to see the layout conform to smaller screens.</p>
+<p><a id="sldbt-blk-2" class="secondary small button" href="http://glitterandmud.com" target="_blank">Take a Closer Look »</a></p>
+						</div>
+						<div class="large-6 columns">
+							<div class="row">
+								<div class="small-4 columns"><img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/gm-mobile.jpg" alt="g&amp;m-mobile" /></div>
+								<div class="small-8 columns">
+
+									<img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/gm-thmb.jpg" alt="g&amp;m-thmb" />
+									<div class="websites-thmb-tri"></div>
+								</div>
+							</div>
+						</div>
+					</article>
+
+					<hr />
+
+					<article class="website-example row wide">
+						<div class="large-6 columns">
+							<h3>WildLuxe</h3>
+<p>Follow Misha into the wild as she shares her photos, videos, guides, and tips on the best adventures around the globe. This blog is driven by a love for adventure travel and fashion and looks exceptionally well on smartphones, tablets, laptops and desktop computers. The site uses sleek techniques to get users where they need to go on a small device. For example, we employ a multi-level drop down navigation to achieve a good user flow.</p>
+<p><a id="sldbt-blk-2" class="secondary small button" href="http://wildluxe.com/" target="_blank">Take a Closer Look »</a></p>
+						</div>
+						<div class="large-6 columns">
+							<div class="row">
+								<div class="small-4 columns"><img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/WildLuxe-mobile.jpg" alt="WildLuxe-mobile" /></div>
+								<div class="small-8 columns"><img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/WildLuxe.jpg" alt="WildLuxe" /></div>
+							</div>
+						</div>
+					</article>
+
+					<hr />
+
+					<article class="website-example row wide">
+						<div class="large-6 columns">
+							<h3>Your Life by Design</h3>
+<p>Your Life by Design is built on a solid foundation of responsive design, satisfying visitors ability to navigate and read page content no matter what size device they use. Check out through the eCommerce store is fast and easy. On mobile screens the hamburger menu icon slides when tapped.</p>
+							<a id="sldbt-blk-2" class="secondary small button" href="http://yourlifebydesignbook.com/" target="_blank">Take a Closer Look »</a>
+
+						</div>
+						<div class="large-6 columns">
+							<div class="row">
+								<div class="small-4 columns"><img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/ylbd-mobile.jpg" alt="ylbd-mobile" /></div>
+								<div class="small-8 columns"><img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/Your-Life-by-Design.jpg" alt="Your-Life-by-Design" /></div>
+							</div>
+						</div>
+					</article>
+
+					<hr />
+
+					<article class="website-example row wide">
+						<div class="large-6 columns">
+							<h3>Justyn Clark Fitness</h3>
+<p>This site strikes a balance between simple and simplistic. A scratch built HTML site with tasteful functionality and subtle animations. A constant "hamburger" design triggers a drop down overlay revealing the site navigation. Mobilized to perfection to make user engagement pleasant across multiple desktop and mobile devices. Web Application functionality slated in continuing development of this site.</p>
+<p><a id="sldbt-blk-2" class="secondary small button" href="http://justynclarkfitness.com/" target="_blank">Take a Closer Look »</a></p>
+						</div>
+						<div class="large-6 columns">
+							<div class="row">
+								<div class="small-4 columns"><img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/JustynClarkFitness-mobile.jpg" alt="JustynClarkFitness-mobile" /></div>
+								<div class="small-8 columns"><img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/JustynClarkFitness.jpg" alt="JustynClarkFitness" /></div>
+							</div>
+						</div>
+					</article>
+
+					<hr />
+
+					<article class="website-example row wide">
+						<div class="large-6 columns">
+							<h3>See Hollywood Tours</h3>
+<p>A simple brochure style site custom built of static responsive HTML5 and CSS3 pages with smooth CSS3 navigation animations. The intended purposed was to allow Los Angeles tourists and visitors to get a glimpse of fun Hollywood attractions and the information to be able to see them. </p>
+<p><a id="sldbt-blk-2" class="secondary small button" href="http://seehollywoodtours.com/" target="_blank">Take a Closer Look »</a></p>
+						</div>
+						<div class="large-6 columns">
+							<div class="row">
+								<div class="small-4 columns"><img class="alignnone" src="/wp-content/themes/justynclark/assets/images/SHT-Mobile.jpg" alt="See Hollywood Tours" width="300" height="470" /></div>
+								<div class="small-8 columns"><img class="alignnone" src="/wp-content/themes/justynclark/assets/images/SHT-Desktop.jpg" alt="scc" /></div>
+							</div>
+						</div>
+					</article>
+
+					<hr />
+
+					<article class="website-example row wide">
+						<div class="large-6 columns">
+							<h3>Shelly Cannon Creative</h3>
+<p>This site strikes a balance between simple and simplistic: The images and text speak for themselves. On mobile devices, the menu slides in from the top and the portfolio displays the popular masonry style layout and responsive animation transitions.</p>
+<p><a id="sldbt-blk-2" class="secondary small button" href="http://www.shellycannoncreative.com/" target="_blank">Take a Closer Look »</a></p>
+						</div>
+						<div class="large-6 columns">
+							<div class="row">
+								<div class="small-4 columns"><img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/ShellyCannonCreativeMobile.jpg" alt="ShellyCannonCreativeMobile" width="300" height="470" /></div>
+								<div class="small-8 columns"><img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/scc.jpg" alt="scc" /></div>
+							</div>
+						</div>
+					</article>
+
+					<hr />
+
+					<article class="website-example row wide">
+						<div class="large-6 columns">
+							<h3>Geffen Playhouse Blog</h3>
+<p>This custom WordPress blog was created from scratch to replicate the exact design and look of the company's main site. On mobile devices, the off-canvas menu slides in from the left.</p>
+<p><a id="sldbt-blk-2" class="secondary small button" href="http://geffenplayhouseblog.com/" target="_blank">Take a Closer Look »</a></p>
+						</div>
+						<div class="large-6 columns">
+							<div class="row">
+								<div class="small-4 columns"><img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/geffenMobile.jpg" alt="geffenMobile" /></div>
+								<div class="small-8 columns"><img class="alignnone" src="http://www.justynclark.com/wp-content/uploads/2015/01/geffenblog.jpg" alt="geffenblog" width="647" height="470" /></div>
+							</div>
+						</div>
+					</article></section>
+
+			</main> <!-- end #main -->
+		    
+		</div> <!-- end #inner-content -->
+	
+	</div> <!-- end #content -->
+
+<?php get_footer(); ?>
