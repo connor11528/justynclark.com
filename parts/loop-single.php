3e71c6cc5ebf725adc28466bfe2cbaf1220d9fde
ijustynclark@gmail.com <jjohnson@historynet.com>
2016-09-19 07:59:00 -0700

    Move single post byline below content

--- parts/loop-single.php
+++ parts/loop-single.php
@@ -1,13 +1,13 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-						
+
 	<header class="article-header">	
 		<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
-		<?php get_template_part( 'parts/content', 'byline' ); ?>
     </header> <!-- end article header -->
 					
     <section class="entry-content" itemprop="articleBody">
 		<?php the_post_thumbnail('full'); ?>
 		<?php the_content(); ?>
+		<?php get_template_part( 'parts/content', 'byline' ); ?>
 	</section> <!-- end article section -->
 						
 	<footer class="article-footer">
